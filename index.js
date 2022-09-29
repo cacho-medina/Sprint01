61aabf0e298509e88c64c9b180b24458a80b701a
Cacho Medina <zanlorenzocacho10@gmail.com>
2022-09-29 01:16:43 -0300

    mobile and tablet done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+let entrada = document.getElementById("ingresar_texto");
+let secundario = document.querySelector(".secondary");
+let main = document.querySelector(".main");
+let padre = document.getElementById("mostrar_texto");
 
-let entrada = document.getElementById('ingresar_texto')
-
-function encriptar(){
+function encriptar() {
+    padre.removeChild(secundario);
+    main.innerHTML = entrada.value;
+    main.className = "salida";
 }
